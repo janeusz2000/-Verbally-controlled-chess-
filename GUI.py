9e47284bdf581bbe671de5830f5028bce34fdd2f
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2018-12-22 22:53:41 +0100

    new_instance of project

---
+++ GUI.py
@@ -0,0 +1,55 @@
+<<<<<<< HEAD
+import pygame, sys
+import ChessEngine
+
+class GUI(object):
+    def __init__(self):
+        pass
+
+
+    def read_game(self, chess_board):
+        pass
+
+#     def __init__(self):
+#         self.screen_ = pygame.display.set_mode([800, 800])
+#
+#
+# while True:
+#
+#
+#
+#
+#
+#
+#     # EVENTS
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             sys.exit(0)
+=======
+import pygame, sys
+import ChessEngine
+
+class GUI(object):
+    def __init__(self):
+        pass
+
+
+    def read_game(self, chess_board):
+        pass
+
+#     def __init__(self):
+#         self.screen_ = pygame.display.set_mode([800, 800])
+#
+#
+# while True:
+#
+#
+#
+#
+#
+#
+#     # EVENTS
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             sys.exit(0)
+>>>>>>> d269694eea858310c264b6adf698d61bfd150415
---
+++ Listener.py
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+"""This in listener who so gonna react to commends"""
+
+
+class Listener(object):
+
+    # constructor
+    def __init__(self):
+        self.exit_game_ = False
+
+    # temporary managiing to end program
+    def exit_game(self):
+        self.exit_game_ = True
+
+    def exit_variable(self):
+        return self.exit_game_
+=======
+"""This in listener who so gonna react to commends"""
+
+
+class Listener(object):
+
+    # constructor
+    def __init__(self):
+        self.exit_game_ = False
+
+    # temporary managiing to end program
+    def exit_game(self):
+        self.exit_game_ = True
+
+    def exit_variable(self):
+        return self.exit_game_
+>>>>>>> d269694eea858310c264b6adf698d61bfd150415

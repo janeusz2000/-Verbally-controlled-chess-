2a600015fbaa3979a0ab29f5be5346d5ab7ad632
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2018-12-22 23:03:45 +0100

    repairing all stuff

--- Listener.py
+++ Listener.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """This in listener who so gonna react to commends"""
 
 
@@ -14,20 +13,3 @@ class Listener(object):
 
     def exit_variable(self):
         return self.exit_game_
-=======
-"""This in listener who so gonna react to commends"""
-
-
-class Listener(object):
-
-    # constructor
-    def __init__(self):
-        self.exit_game_ = False
-
-    # temporary managiing to end program
-    def exit_game(self):
-        self.exit_game_ = True
-
-    def exit_variable(self):
-        return self.exit_game_
->>>>>>> d269694eea858310c264b6adf698d61bfd150415
--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 """ initializating program from here"""
 
 import Commander
 
 GameCommander = Commander.Commander()
 GameCommander.game_run(iteration_number=5)
-=======
-""" initializating program from here"""
-
-import Commander
-
-GameCommander = Commander.Commander()
-GameCommander.game_run(iteration_number=5)
->>>>>>> d269694eea858310c264b6adf698d61bfd150415

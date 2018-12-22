93a46218a7f4d9af51cecec16d39175d887fc472
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2018-12-23 00:28:52 +0100

    adding chess board

--- Commander.py
+++ Commander.py
@@ -10,26 +10,33 @@ class Commander(object):
     # constructor
     def __init__(self):
         self.chess_engine_ = ChessEngine.ChessEngine(True)
-        self.gui_ = GUI.GUI()
         self.listener_ = Listener.Listener()
+        self.gui_ = GUI.GUI()
 
     def game_run(self, iteration_number):
 
-        # main game loop
         local_iteration = 1
+
         while True:
+
+            self.gui_.load_texture()
+            self.gui_.check_events()
+            self.gui_.update_screen()
+
 
             self.gui_.read_game(chess_board=self.chess_engine_)
 
-            # temporary console view
-            self.chess_engine_.console_view()
-            self.chess_engine_.move()
+            # =====================================
 
-            # ENDING PROGRAM HERE
-            if local_iteration >= iteration_number:
-                self.listener_.exit_game()
-            else:
-                local_iteration += 1
+            # # temporary console view
+            # self.chess_engine_.console_view()
+            # self.chess_engine_.move()
+            #
+            # # ENDING PROGRAM HERE
+            # if local_iteration >= iteration_number:
+            #     self.listener_.exit_game()
+            # else:
+            #     local_iteration += 1
 
 
 
--- GUI.py
+++ GUI.py
@@ -1,13 +1,31 @@
-import pygame, sys
+import pygame
+import sys
 import ChessEngine
 
 class GUI(object):
     def __init__(self):
-        pass
+        pygame.init()
+        self.screen_ = pygame.display.set_mode([800, 800])
 
+    def __del__(self):
+        pygame.quit()
 
     def read_game(self, chess_board):
         pass
+
+    @staticmethod
+    def check_events():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+    def load_texture(self):
+        self.screen_.fill((0, 0, 0))
+        self.screen_.blit(pygame.image.load('ChessArt/texture.png').convert(), (0, 0))
+
+
+    def update_screen(self):
+        pygame.display.update()
 
 #     def __init__(self):
 #         self.screen_ = pygame.display.set_mode([800, 800])

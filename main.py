10de00c1de2f85fea53b062696846d176c6f691e
Natquuu <nataliahebda7@gmail.com>
2018-12-29 20:03:46 +0100

    poddanie,koniec gry

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import Listener
 #c = ChessConverter.ChessConverter()
 #converted = c.convert("skoczek ef pięć bije a trzy")
 #con = c.convert("pionek gie sześć bije ef siedem")
-l = Listener.Listener()
-l.listen()
-# GameCommander = Commander.Commander()
-# GameCommander.game_run(iteration_number=15) # iteration number = ile łacznie zostanie wykonanych ruchów
+# l = Listener.Listener()
+# l.listen()
+GameCommander = Commander.Commander()
+GameCommander.game_run(iteration_number=15) # iteration number = ile łacznie zostanie wykonanych ruchów

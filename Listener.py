22d1bef97b80482b6fa161467417921a43d3c794
Aga <agnieszka.fatla.1997@gmail.com>
2019-01-09 22:06:28 +0100

    finally closing instructions by voice

--- Listener.py
+++ Listener.py
@@ -4,7 +4,6 @@ import run_sarmata
 import keyboard
 import Instructions
 import ChessConverter
-import sys
 
 
 class Listener(object):
@@ -19,24 +18,28 @@ class Listener(object):
     # temporary managing to end program
     def listen(self):
         instructions = Instructions.Instructions("instructions_pl.txt", "instrukcje gry")
-        try:
-            if keyboard.is_pressed('space'):
-                self.move_ = ''
-                os.system('python run_sarmata.py')
-                f = open("result.txt", "r")
-                self.message_ = f.read()
-                if self.message_ == "otwórz instrukcje" or self.message_ == "pokaż instrukcje":
-                    instructions.show_instructions()
-                elif self.message_ == "zamknij instrukcje":
-                    instructions.close_instructions()
-                elif self.message_ == "poddaję się" or self.message_ == "poddaj się" \
-                        or self.message_ == "zakończ grę" or self.message_ == "koniec gry" or self.message_ == "koniec":
-                    sys.exit(1)
+        while True:  # making a loop
+            try:
+                if keyboard.is_pressed('space'):
+                    self.move_ = ''
+                    os.system('python run_sarmata.py')
+                    f = open("result.txt", "r")
+                    self.message_ = f.read()
+                    if self.message_ == "otwórz instrukcje" or self.message_ == "pokaż instrukcje":
+                        instructions.show_instructions()
+                    elif self.message_ == "zamknij instrukcje":
+                        instructions.close_instructions()
+                    elif self.message_ == "poddaję się" or self.message_ == "poddaj się" \
+                            or self.message_ == "zakończ grę" or self.message_ == "koniec gry" or self.message_ == "koniec":
+                        print("exit") # jeszcze trzeba połączyc z funkcja exit
+                    else:
+                        self.move_ = self.converter_.convert(c=self.message_)
+                    f.close()
+                    break
                 else:
-                    self.move_ = self.converter_.convert(c=self.message_)
-                f.close()
-        except:
-            pass
+                    pass
+            except:
+                break
 
     def exit_game(self):
         self.exit_game_ = True

36ad3a9233e08fb35d14547ba111244c0436d369
janeusz2000 <45213495+janeusz2000@users.noreply.github.com>
2019-01-09 21:20:12 +0100

    update_to smooth_window

--- Listener.py
+++ Listener.py
@@ -18,28 +18,24 @@ class Listener(object):
     # temporary managing to end program
     def listen(self):
         instructions = Instructions.Instructions("instructions_pl.txt", "instrukcje gry")
-        while True:  # making a loop
-            try:
-                if keyboard.is_pressed('space'):
-                    self.move_ = ''
-                    os.system('python run_sarmata.py')
-                    f = open("result.txt", "r")
-                    self.message_ = f.read()
-                    if self.message_ == "otwórz instrukcje" or self.message_ == "pokaż instrukcje":
-                        instructions.show_instructions()
-                    elif self.message_ == "zamknij instrukcje":
-                        instructions.close_instructions()
-                    elif self.message_ == "poddaję się" or self.message_ == "poddaj się" \
-                            or self.message_ == "zakończ grę" or self.message_ == "koniec gry" or self.message_ == "koniec":
-                        print("exit") # jeszcze trzeba połączyc z funkcja exit
-                    else:
-                        self.move_ = self.converter_.convert(c=self.message_)
-                    f.close()
-                    break
+        try:
+            if keyboard.is_pressed('space'):
+                self.move_ = ''
+                os.system('python run_sarmata.py')
+                f = open("result.txt", "r")
+                self.message_ = f.read()
+                if self.message_ == "otwórz instrukcje" or self.message_ == "pokaż instrukcje":
+                    instructions.show_instructions()
+                elif self.message_ == "zamknij instrukcje":
+                    instructions.close_instructions()
+                elif self.message_ == "poddaję się" or self.message_ == "poddaj się" \
+                        or self.message_ == "zakończ grę" or self.message_ == "koniec gry" or self.message_ == "koniec":
+                    print("exit") # jeszcze trzeba połączyc z funkcja exit
                 else:
-                    pass
-            except:
-                break
+                    self.move_ = self.converter_.convert(c=self.message_)
+                f.close()
+        except:
+            pass
 
     def exit_game(self):
         self.exit_game_ = True

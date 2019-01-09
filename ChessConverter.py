77a17834353f0eebd5ef21bc8e619dc958d52d3b
Aga <agnieszka.fatla.1997@gmail.com>
2019-01-09 15:01:01 +0100

    old converteer

--- ChessConverter.py
+++ ChessConverter.py
@@ -5,116 +5,79 @@ class ChessConverter(object):
 
     # constructor
     def __init__(self):
-        print("converter inited")
+        print("converter")
 
     def convert(self, c):
         converted = ''
+        # splitting to words "pionek e trzy na/zbij e cztery"
         command = c.split()
+
         if c == "roszada w stronę króla":
             converted += 'O-O'
         elif c == "roszada w stronę królowej":
             converted += 'O-O-O'
-        elif command[0] == "wieża":
-            converted += 'R'
-        elif command[0] == "skoczek":
-            converted += 'N'
-        elif command[0] == "laufer" or command[0] == "goniec":
-            converted += 'B'
-        elif command[0] == "królowa" or command[0] == "hetman":
-            converted += 'Q'
-        elif command[0] == "król":
-            converted += 'K'
-        elif command[0] == "pionek":
-            converted = converted
         else:
-            print("Wrong command")
-        if command[3] == "zbij" or command[3] == "bije" or command[3] == "bij" or command[3] == "zbija":
-            if command[1] == "a":
+            if command[0] == "wieża":
+                converted += 'R'
+            elif command[0] == "skoczek":
+                converted += 'N'
+            elif command[0] == "laufer" or command[0] == "goniec":
+                converted += 'B'
+            elif command[0] == "królowa" or command[0] == "hetman":
+                converted += 'Q'
+            elif command[0] == "król":
+                converted += 'K'
+            elif command[0] == "pionek":
+                converted = converted
+            if command[3] == "zbij" or command[3] == "bije" or command[3] == "bij" or command[3] == "zbija":
+                if command[1] == "a":
+                    converted += 'a'
+                elif command[1] == "be":
+                    converted += 'b'
+                elif command[1] == "ce":
+                    converted += 'c'
+                elif command[1] == "de":
+                    converted += 'd'
+                elif command[1] == "e":
+                    converted += 'e'
+                elif command[1] == "ef":
+                    converted += 'f'
+                elif command[1] == "gie":
+                    converted += 'g'
+                elif command[1] == "ha":
+                    converted += 'h'
+                converted += "x"
+            if command[4] == "a":
                 converted += 'a'
-            elif command[1] == "be":
+            elif command[4] == "be":
                 converted += 'b'
-            elif command[1] == "ce":
+            elif command[4] == "ce":
                 converted += 'c'
-            elif command[1] == "de":
+            elif command[4] == "de":
                 converted += 'd'
-            elif command[1] == "e":
+            elif command[4] == "e":
                 converted += 'e'
-            elif command[1] == "ef":
+            elif command[4] == "ef":
                 converted += 'f'
-            elif command[1] == "gie":
+            elif command[4] == "gie":
                 converted += 'g'
-            elif command[1] == "ha":
+            elif command[4] == "h":
                 converted += 'h'
-            else:
-                print("Wrong command")
-            converted += "x"
-        elif command[3] == "na" and command[0] != "pionek":
-            if command[1] == "a":
-                converted += 'a'
-            elif command[1] == "be":
-                converted += 'b'
-            elif command[1] == "ce":
-                converted += 'c'
-            elif command[1] == "de":
-                converted += 'd'
-            elif command[1] == "e":
-                converted += 'e'
-            elif command[1] == "ef":
-                converted += 'f'
-            elif command[1] == "gie":
-                converted += 'g'
-            elif command[1] == "ha":
-                converted += 'h'
-            if command[2] == "jeden":
+            if command[5] == "jeden":
                 converted += "1"
-            if command[2] == "dwa":
+            if command[5] == "dwa":
                 converted += "2"
-            if command[2] == "trzy":
+            if command[5] == "trzy":
                 converted += "3"
-            if command[2] == "cztery":
+            if command[5] == "cztery":
                 converted += "4"
-            if command[2] == "pięć":
+            if command[5] == "pięć":
                 converted += "5"
-            if command[2] == "sześć":
+            if command[5] == "sześć":
                 converted += "6"
-            if command[2] == "siedem":
+            if command[5] == "siedem":
                 converted += "7"
-            if command[2] == "osiem":
+            if command[5] == "osiem":
                 converted += "8"
-        if command[4] == "a":
-            converted += 'a'
-        elif command[4] == "be":
-            converted += 'b'
-        elif command[4] == "ce":
-            converted += 'c'
-        elif command[4] == "de":
-            converted += 'd'
-        elif command[4] == "e":
-            converted += 'e'
-        elif command[4] == "ef":
-            converted += 'f'
-        elif command[4] == "gie":
-            converted += 'g'
-        elif command[4] == "ha":
-            converted += 'h'
-        else:
-            print("Wrong command")
-        if command[5] == "jeden":
-            converted += "1"
-        if command[5] == "dwa":
-            converted += "2"
-        if command[5] == "trzy":
-            converted += "3"
-        if command[5] == "cztery":
-            converted += "4"
-        if command[5] == "pięć":
-            converted += "5"
-        if command[5] == "sześć":
-            converted += "6"
-        if command[5] == "siedem":
-            converted += "7"
-        if command[5] == "osiem":
-            converted += "8"
-
 
         return converted

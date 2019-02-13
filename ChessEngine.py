2646348c3647bd6c5ab3f79b467b85b891d224a7
Natquuu <nataliahebda7@gmail.com>
2019-02-13 19:42:04 +0100

    chooseing figure on the end of board

--- ChessEngine.py
+++ ChessEngine.py
@@ -10,7 +10,7 @@ class ChessEngine:
     # constructor
 
     def __init__(self, manual_input):
-        self.board_ = chess.Board()
+        self.board_ = chess.Board("rnbq1bnr/pppPk1pp/8/8/8/4p3/PPP2PPP/RNBQKBNR w KQ - 1 6")
         self.stalemate_ = False
         self.draw_ = False
         self.mate_ = False
@@ -46,7 +46,8 @@ class ChessEngine:
                     if self.board_.is_check():
                         winsound.PlaySound("check_pl.wav", winsound.SND_FILENAME)
                 except ValueError:
-                    winsound.PlaySound("wrong_move_pl.wav", winsound.SND_FILENAME)
+                    if self.is_end_of_board() == False:
+                        winsound.PlaySound("wrong_move_pl.wav", winsound.SND_FILENAME)
 
 
     def console_view(self):
@@ -97,3 +98,25 @@ class ChessEngine:
                 list_of_moves.append(move.uci()[2:])
         self.to_where_ = list_of_moves
 
+    def is_end_of_board(self):
+        moves = self.board_.legal_moves
+        move_ = self.listener_.move_
+        move_.replace(move_[1], "x")
+        change = False
+        if len(move_) == 4:
+            for move in moves:
+                if move_ in move.uci():
+                    winsound.PlaySound("ask_for_figure_pl.wav", winsound.SND_FILENAME)
+                    self.listener_.listen()
+                    move_ = move_ + "=" + self.listener_.move_
+                    try:
+                        print(move_)
+                        self.board_.push_san(move_)
+                        change = True
+                        self.listener_.move_ = ''
+                        if self.board_.is_check():
+                            winsound.PlaySound("check_pl.wav", winsound.SND_FILENAME)
+                        break
+                    except ValueError:
+                        winsound.PlaySound("wrong_move_pl.wav", winsound.SND_FILENAME)
+        return change
